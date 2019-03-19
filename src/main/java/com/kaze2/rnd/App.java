022f8975a97c5edfdcde645dd3fee83addf88562
000407 <senad.407@gmail.com>
2019-03-19 16:58:12 +0530

    Initial commit

---
+++ src/main/java/com/kaze2/rnd/App.java
@@ -0,0 +1,15 @@
+package com.kaze2.rnd;
+
+import com.kaze2.rnd.hashes.MD5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MD5.generate("AJN");
+    }
+}
